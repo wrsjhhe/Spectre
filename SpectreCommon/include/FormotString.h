f32a1314d0899be72b96d0e361ee9d92b55e1aa4
zd <chainzhang@aliyun.com>
2022-01-13 20:58:07 +0800

    字符串拼接方法

---
+++ SpectreCommon/include/FormotString.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "CommonDefinitions.h"
+#include <sstream>
+
+BEGIN_NAMESPACE_COMMON
+
+template <typename SSType>
+void FormatStrSS(SSType& ss)
+{
+}
+
+template <typename SSType, typename ArgType>
+void FormatStrSS(SSType& ss, const ArgType& Arg)
+{
+    ss << Arg;
+}
+
+template <typename SSType, typename FirstArgType, typename... RestArgsType>
+void FormatStrSS(SSType& ss, const FirstArgType& FirstArg, const RestArgsType&... RestArgs)
+{
+    FormatStrSS(ss, FirstArg);
+    FormatStrSS(ss, RestArgs...); // recursive call using pack expansion syntax
+}
+
+template <typename... RestArgsType>
+std::string FormatString(const RestArgsType&... Args)
+{
+    std::stringstream ss;
+    FormatStrSS(ss, Args...);
+    return ss.str();
+}
+
+END_NAMESPACE_COMMON
