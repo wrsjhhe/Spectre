cc80863f303c6b5bb2b512e279645128ea876d71
chain <chainzhang@aliyun.com>
2022-01-22 19:30:35 +0800

    修改LogTest错误

--- tests/logTest.cpp
+++ tests/logTest.cpp
@@ -11,9 +11,9 @@ TEST(TestLog, test1)
     LOG_INFO("info message");
     LOG_WARN("warn message");
     LOG_ERROR("error message");
-    LOG_DEBUG_FMT("[{}:{}]", __FILE__, __LINE__);
-    LOG_INFO_FMT("[{}:{}]", __FILE__, __LINE__);
-    LOG_WARN_FMT("[{}:{}]", __FILE__, __LINE__);
-    LOG_ERROR_FMT("[{}:{}]", __FILE__, __LINE__);
+    LOG_DEBUG_FMT("[{}:{}]{}", __FILE__, __LINE__, "debug message");
+    LOG_INFO_FMT("[{}:{}]{}", __FILE__, __LINE__, "info message");
+    LOG_WARN_FMT("[{}:{}]{}", __FILE__, __LINE__, "warn message");
+    LOG_ERROR_FMT("[{}:{}]{}", __FILE__, __LINE__, "error message");
     EXP_CHECK(0, "exp check faild");
 }
